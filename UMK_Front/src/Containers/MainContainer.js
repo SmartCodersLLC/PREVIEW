c94769bd201a8c7619c468ac124fb7806d6fbb74
Toichubek5 <169206.tz@gmail.com>
2022-04-11 17:19:58 +0600

    end

--- UMK_Front/src/Containers/MainContainer.js
+++ UMK_Front/src/Containers/MainContainer.js
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useResetRecoilState } from "recoil";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { userState } from "../State/user";
 import { appName } from "../Service/http";
 import MenuItem from "../Components/Menu/Item";
 import MenuWrapper from "../Components/Menu/Wrapper";
-import Language from "../Components/Language";
+import Button from "../Components/Button";
+import { AuthService } from "../Service/auth";
+import { notify } from "../Utils/notify";
 
 export function MainContainer() {
   const [hovered, setHover] = useState(0);
   const user = useRecoilValue(userState);
+  const resetUser = useResetRecoilState(userState);
   const { t } = useTranslation();
   const navigate = useNavigate();
 
   const MenuList = [{ id: 1, path: "umk", title: t("head.umk") }];
+
+  const logoutHandler = async () => {
+    const { data, error, message } = await AuthService.logout();
+    if (error) {
+      console.log(message);
+    }
+    if (data) {
+      notify(message);
+    }
+    resetUser();
+    navigate(`${appName}/login`);
+  };
 
   if (!user.isAuthenticated) {
     navigate(`${appName}/login`);
   }
   return (
     <>
-      <Language />
+      <Button logoutHandler={logoutHandler} logoutText={t("logout")} />
       <MenuWrapper>
         {MenuList.map((menu) => {
           return (
